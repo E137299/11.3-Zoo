38119af6dcaad80bb7cee5243257170542eadfc1
David Yin <29613693+E137299@users.noreply.github.com>
2025-02-03 14:28:39 -0600

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+
+// TODO: extend Entity
+// TODO: make Animal abstract
+public class Animal {
+
+    // TODO: instance variables
+
+    // TODO: add constructor
+
+    // TODO: add abstract method eat(Food food)
+    // TODO: add abstract method move(Zoo zoo)
+    
+    // TODO: add non-abstact methods as necessary
+}
