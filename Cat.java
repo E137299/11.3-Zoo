2d0d88011d3803c6b3cf78b87845c0479f7bce67
David Yin <29613693+E137299@users.noreply.github.com>
2025-02-03 14:19:13 -0600

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+import java.awt.*;
+
+// TODO: extend Animal
+public class Cat {
+
+    // TODO: add instance variables
+
+    // TODO: add constructor
+
+    // TODO: override the tick method
+
+    @Override
+    public void draw(Graphics g) {
+        // two optional examples of a way to draw a cat follow!
+
+        //g.setColor(Color.DARK_GRAY);
+        //g.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 25));
+        //g.drawString("🐈", Zoo.wrap(xPos,Zoo.ZOO_COLS)*Zoo.SCALE, Zoo.wrap(yPos,Zoo.ZOO_ROWS)*Zoo.SCALE+25);
+
+        //g.setColor(Color.DARK_GRAY);
+        //g.setFont(new Font("Consolas", Font.PLAIN, 10));
+        //g.drawString(" ^-^ ", Zoo.wrap(xPos,Zoo.ZOO_COLS)*Zoo.SCALE, Zoo.wrap(yPos,Zoo.ZOO_ROWS)*Zoo.SCALE+5);
+        //g.drawString("/. .\\", Zoo.wrap(xPos,Zoo.ZOO_COLS)*Zoo.SCALE, Zoo.wrap(yPos,Zoo.ZOO_ROWS)*Zoo.SCALE+15);
+        //g.drawString("\\_o_/", Zoo.wrap(xPos,Zoo.ZOO_COLS)*Zoo.SCALE, Zoo.wrap(yPos,Zoo.ZOO_ROWS)*Zoo.SCALE+25);
+    }
+
+    // TODO: override the eat method
+    // TODO: override the move method
+
+
+}
