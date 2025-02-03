8f95bed278ed1226ffab14ec4e2f1fc67ffd92ab
David Yin <29613693+E137299@users.noreply.github.com>
2025-02-03 14:17:34 -0600

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,14 @@
+
+// TODO: make Food abstract
+// TODO: extend Entity
+public class Food {
+
+    // TODO: add instance variables
+
+    // TODO: add constructor
+
+    // TODO: add abstract method beEaten(Animal eater)
+
+    // TODO: add non-abstract methods as needed
+
+}
